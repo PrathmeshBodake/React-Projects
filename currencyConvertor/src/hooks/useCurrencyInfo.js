2e989a8f3fef5772deda27b6844a81d907ac6218
PrathmeshBodake <prathmeshbodake007@gmail.com>
2026-03-02 13:03:12 +0530

    Add custom hook to fetch currency exchange rates

--- currencyConvertor/src/hooks/useCurrencyInfo.js
+++ currencyConvertor/src/hooks/useCurrencyInfo.js
@@ -1,17 +1,19 @@
-import {useEffect , useState} from 'react';
+import { useEffect, useState } from "react";
 
-function useCurrencyInfo(currency){
-    const [data , setData] = useState({});
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
 
-    useEffect(()=>{
-       
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res[currency]);
+      });
+  }, [currency]);
 
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
-        .then(res => res.json())
-        .then(res => setData(res[currency]));
-
-        console.log(data);
-    },[currency]);
-    console.log(data);
-    return data;
+  return data;
 }
+
+export default useCurrencyInfo;
