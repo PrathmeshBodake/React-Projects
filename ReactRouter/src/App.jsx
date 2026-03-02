28dea217154e7b322c8693099dc45d55973c02e1
PrathmeshBodake <prathmeshbodake007@gmail.com>
2026-03-02 15:06:44 +0530

    Project setup

---
+++ ReactRouter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='text-3xl font-bold text-center bg-gray-800 text-white'>React Router</h1>
+      
+    </>
+  )
+}
+
+export default App
