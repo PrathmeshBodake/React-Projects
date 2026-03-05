f75fdf5aaf4bc58d02139ca80d0346e43972aee0
PrathmeshBodake <prathmeshbodake007@gmail.com>
2026-03-05 10:09:10 +0530

    Initialising project

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Header from './components/Header/Header'
 
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
   return (
     <>
     <h1 className='text-3xl font-bold text-center bg-gray-800 text-white'>React Router</h1>
-      
+    <Header />
     </>
   )
 }
