b95ecc570a2ea6c3d8e1aed58ac3ee692f72b5a2
PrathmeshBodake <prathmeshbodake007@gmail.com>
2026-03-06 17:30:06 +0530

    File who ran userContextProvider and components

--- MiniContext/src/App.jsx
+++ MiniContext/src/App.jsx
@@ -1,12 +1,17 @@
-
+import UserContextProvider from "./context/userContextProvider"
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
   
 
   return (
-    <>
-      
-    </>
+    <div className="h-screen w-screen bg-slate-200 flex flex-col justify-center items-center gap-y-10">
+      <UserContextProvider>
+        <Login/>
+        <Profile/>
+      </UserContextProvider>
+    </div>
   )
 }
 
